f7611fc42ca3da8a4f01ba493a696f847636d5fd
Nikhil Aggarwal <nikhigrwl40@gmail.com>
2021-12-03 22:33:55 +0530

    getitems

--- api/views.py
+++ api/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from .serializers import ItemsSerializer
+from .models import items
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls = {
+        'All Items': '/items',
+        'Search by Category': '/category',
+        'Search by Subcategory': '/subcategory',
+        'Add': '/add',
+        'Update': '/update'
+    }
+    return Response(api_urls)
+
+@api_view(['GET'])
+def getItems(request):
+    if request.query_params:
+        print(request.query_params)
+        Items=items.objects.filter(**request.query_params.dict())
+        print(Items)
+        # return Response({})
+    else:
+        Items = items.objects.all()
+    serializer = ItemsSerializer(Items, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def filterItems(request):
+    pass
