2e997577a0dab3251745e7008072e791dfd40fec
Nikhil Aggarwal <nikhigrwl40@gmail.com>
2021-12-03 17:54:13 +0530

    models and serializers

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class items(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    subcategory = models.CharField(max_length=255)
+    amount = models.PositiveIntegerField()
+
+    def __str__(self) -> str:
+        return self.name + "|" + self.category + "|" + self.subcategory
---
+++ api/serializers.py
@@ -0,0 +1,8 @@
+from django.db.models import fields
+from rest_framework import serializers
+from .models import items
+
+class ItemsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = items
+        fields = '__all__'
