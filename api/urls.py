0113844875405728cdebd464ce84970baabb4c77
Nikhil Aggarwal <nikhigrwl40@gmail.com>
2021-12-03 23:14:20 +0530

    addItems

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.apiOverview, name='api-overview'),
-    path('items/', views.getItems, name='get-items')
+    path('items/', views.getItems, name='get-items'),
+    path('add/', views.addItems, name='add-items')
 ]
--- api/views.py
+++ api/views.py
@@ -3,13 +3,15 @@ from .serializers import ItemsSerializer
 from .models import items
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import serializers
 
 @api_view(['GET'])
 def apiOverview(request):
     api_urls = {
-        'All Items': '/items',
-        'Search by Category': '/category',
-        'Search by Subcategory': '/subcategory',
+        'All Items': '/items/',
+        'Search by Category': '/items/?category=category_name',
+        'Search by Subcategory': '/items/?subcategory=category_name',
         'Add': '/add',
         'Update': '/update'
     }
@@ -18,15 +20,24 @@ def apiOverview(request):
 @api_view(['GET'])
 def getItems(request):
     if request.query_params:
-        print(request.query_params)
         Items=items.objects.filter(**request.query_params.dict())
-        print(Items)
-        # return Response({})
     else:
         Items = items.objects.all()
-    serializer = ItemsSerializer(Items, many=True)
-    return Response(serializer.data)
+    if Items:
+        serializer = ItemsSerializer(Items, many=True)
+        return Response(serializer.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
-@api_view(['GET'])
-def filterItems(request):
-    pass
+@api_view(['POST'])
+def addItems(request):
+    serializer = ItemsSerializer(data=request.data)
+
+    if items.objects.filter(**request.data).exists():
+        raise serializers.ValidationError("This data already exists")
+    
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
